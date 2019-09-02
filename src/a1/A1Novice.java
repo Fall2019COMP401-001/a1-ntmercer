b0b7b84e009ab7e8efbd85a00dd45037d0e3cb3e
ntm19 <ntm19@DESKTOP-58TEAH8>
2019-09-02 01:22:37 -0400

    Added A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -2,13 +2,72 @@ package a1;
 
 import java.util.Scanner;
 
-public class A1Novice {
-
-	public static void main(String[] args) {
+public class A1Novice
+{
+	public static void main(String[] args)
+	{
+		//Create our scanner and necessary outside variables:
+		//An "output" string to keep track of what will be pushed to the console
+		//and a variable to hold the number of customers the program will count
+		Scanner input = new Scanner(System.in);
+		String output = "";
+		int numberOfCustomers;
 		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		//Ask for total number of customers and record value
+		System.out.print("Enter total number of customers >> ");
+		numberOfCustomers = input.nextInt();
 		
+		//This loop runs for each customer and records their shopping data.
+		for(int x = 1; x <= numberOfCustomers; ++x)
+		{
+			//Necessary major variables for customer data:
+			//Variables to hold name
+			//Variable to hold number of items to be tracked
+			//A variable to hold the total price of this customer's order (resets for each customer)
+			String firstName, lastName;
+			int numberOfItems;
+			double totalPrice = 0;
+
+			//Three readings from the console to determine first and last name as well as number of items
+			System.out.print("Enter first name of customer " + x + " >> ");
+			firstName = input.next();
+			System.out.print("Enter last name of customer " + x + " >> ");
+			lastName = input.next();
+			System.out.print("Enter number of items bought by customer " + x + " >> ");
+			numberOfItems = input.nextInt();
+			
+			//This loop runs for each item the customer is buying and records it price.
+			for(int y = 1; y <= numberOfItems; ++y)
+			{
+				//Variables:
+				//One to store name of item
+				//One to store how many of that item is being purchased
+				//One to store the price of ONE of that item
+				String itemName;
+				int quantityOfItem;
+				double itemPrice;
+				
+				//Record the amount of item, name of item, and price of item bought.
+				System.out.print("Enter amount of item " + y + " bought >> ");
+				quantityOfItem = input.nextInt();
+				System.out.print("Enter name of item " + y + " >> ");
+				itemName = input.next();
+				System.out.print("Enter price of item " + y + " (per item) >> ");
+				itemPrice = input.nextDouble();
+				
+				//Accumulate the total price of the order by adding the price of each item after the loop.
+				totalPrice += quantityOfItem * itemPrice;
+			}
+			
+			//Add a line to the output for the current customer which includes all of their data.
+			//Uses the charAt method to locate the first initial of the customer,
+			//and uses the String.format method to configure the final prices.
+			//This runs once for each customer.
+			output += firstName.charAt(0) + ". " + lastName + ": " + String.format("%.2f", totalPrice) + "\n";
+		}
+		
+		//Display the full output to the console, and close input scanner
+		System.out.print(output);
+		input.close();
 	}
 }
